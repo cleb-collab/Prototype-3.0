5f45d799ff9509ec4aeafa5699a7099dd1deee89
cleb-collab <CLeberr@nycstudents.net>
2023-11-27 12:38:32 -0500

    PROTO

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
  public float gravityModifier;
  public bool isOnGround = true;
  public bool gameOver;
+ private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -21,7 +23,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)&& isOnGround) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        isOnGround = false;  }
+        isOnGround = false; playerAnim.SetTrigger("Jump_trig");  }
     }
     private void OnCollisionEnter(Collision collision)
     {
